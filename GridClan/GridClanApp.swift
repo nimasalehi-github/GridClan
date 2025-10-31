41fc795c6409222f22585f0afed11a15cf77a722
n salehi <nimasalehi.auth@icloud.com>
2025-10-31 17:55:30 +0330

    Initial Commit

---
+++ GridClan/GridClanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridClanApp.swift
+//  GridClan
+//
+//  Created by Nima Salehi on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GridClanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
