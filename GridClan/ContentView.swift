1147d3984a2c36f6c29ec74cefa3b3be2e1f7f93
n salehi <nimasalehi.auth@icloud.com>
2025-11-04 23:17:40 +0330

    Restart

--- GridClan/ContentView.swift
+++ GridClan/ContentView.swift
@@ -7,34 +7,15 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//
-//    var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//
-//        }
-//        .padding()
-//    }
-//}
-
 struct ContentView: View {
-    @StateObject var vm = GameGridViewModel()
-    @StateObject var gm = GestureManager()
 
     var body: some View {
         VStack {
-            SmartSuggestionView()
-            ControlPanelView(vm: vm)
-            GameGridView(vm: vm, gm: gm)
-                .frame(width: 400, height: 400)
-                .border(Color.gray)
-            Text("Gesture: \(gm.gestureText)")
-                .font(.footnote)
-                .padding(.top, 6)
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+
         }
         .padding()
     }
